97f3a032f43b901fc56bc638f3af9e25b1dde9f1
Axel Hecht <axel@pike.org>
2012-06-10 14:08:49 +0200

    support logging in, just into session

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
+  app.use(express.session({ secret: 'DoNoTrack' }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -32,6 +32,8 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/profile', routes.profile)
+app.post('/profile', routes.change_profile)
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,45 @@
+var util = require('util');
 
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Localizers and Roles' })
+  var args = extractUser(req);
+  args.title = 'Localizers and Roles';
+  res.render('index', args);
 };
+
+
+/*
+ * GET profile page.
+ */
+
+exports.profile = function(req, res){
+  var args = extractUser(req);
+  args.title = 'Your Profile';
+  args.redirect = req.headers.referer || '/';
+  res.render('profile', args);
+};
+
+
+/*
+ * POST profile page.
+ */
+
+exports.change_profile = function(req, res){
+  req.session.email = req.body.email;
+  req.session.shortname = req.body.shortname;
+  if (req.body.fullname) req.session.fullname = req.body.fullname;
+  res.redirect(req.body.redirect);
+};
+
+
+function extractUser(req, args) {
+  args = args || {};
+  args.email = req.session.email;
+  args.fullname = req.session.fullname;
+  args.shortname = req.session.shortname;
+  args.logged_in = args.email && args.shortname;
+  return args;
+}
